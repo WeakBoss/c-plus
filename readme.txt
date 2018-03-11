96568d0712ea31e99cda08cea6383d2c319f1077
WeakBoss <2366507496@qq.com>
2018-03-11 20:32:11 +0800

    first commit

---
+++ readme.txt
@@ -0,0 +1,4 @@
+I save the code on the Github.
+And I try to use the Git bash.
+
+？？怎么回事
---
+++ 虚函数.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+class A {
+public:
+	A(int x, int y, int z) :x(x), y(y), z(z) {}
+	  void show() const {
+		cout << x << " " << y << " " << z <<" AµÄ"<< endl;
+	}
+protected:
+	int x, y, z;
+};
+class B :public A {
+public:
+	B(int x, int y, int z, int i) :A(x, y, z) {
+		this->i = i;
+	}
+	void show() const {
+		cout << x << " " << y << " " << z << " " << i << " BµÄ"<<endl;
+	}
+private:
+	int i;
+};
+void reference(const A& obj) {
+	obj.show();
+}
+int main()
+{
+	A *pa;
+	A a(1,2,3);
+	B b(4,5,6,7);
+	pa=&a;
+	pa->show();
+	pa=&b;
+	pa->show();	
+	b.show();
+	b.A::show();
+	return 0;
+}
+
